732de8797ae9a56af09d25087804ee596812e003
Leonardo Marques <marqleo@protonmail.com>
2026-09-14 00:10:35 -0300

    feat(nvim): add fzf-lua pickers for marks, resume, and git buffer commits

--- programs/neovim/keymaps.lua
+++ programs/neovim/keymaps.lua
@@ -14,9 +14,7 @@ vim.keymap.set("n", "<leader>|", "<cmd>vsplit<CR>", { silent = true, desc = "Spl
 vim.keymap.set("n", "<leader>-", "<cmd>split<CR>", { silent = true, desc = "Split window horizontally" })
 
 -- LSP
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
 vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename symbol" })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
--- programs/neovim/plugins/fzf-lua.lua
+++ programs/neovim/plugins/fzf-lua.lua
@@ -4,6 +4,30 @@ return {
 		{ "<leader><space>", "<cmd>FzfLua files<CR>", desc = "Find files" },
 		{ "<leader>/", "<cmd>FzfLua live_grep<CR>", desc = "Grep" },
 		{ "<leader>,", "<cmd>FzfLua buffers<CR>", desc = "List buffers" },
+		{ "<leader>R", "<cmd>FzfLua resume<CR>", desc = "Resume last picker" },
+		
+		-- LSP Integrations
+		{ "gd", "<cmd>FzfLua lsp_definitions<CR>", desc = "Go to definition" },
+		{ "gD", "<cmd>FzfLua lsp_declarations<CR>", desc = "Go to declaration" },
+		{ "gi", "<cmd>FzfLua lsp_implementations<CR>", desc = "Go to implementation" },
+		{ "gr", "<cmd>FzfLua lsp_references<CR>", desc = "Go to references" },
+		{ "<leader>ss", "<cmd>FzfLua lsp_document_symbols<CR>", desc = "Document symbols" },
+		{ "<leader>sS", "<cmd>FzfLua lsp_live_workspace_symbols<CR>", desc = "Workspace symbols" },
+		{ "<leader>sd", "<cmd>FzfLua diagnostics_document<CR>", desc = "Document diagnostics" },
+		{ "<leader>sD", "<cmd>FzfLua diagnostics_workspace<CR>", desc = "Workspace diagnostics" },
+		
+		-- Neovim Internals & Help
+		{ "<leader>sh", "<cmd>FzfLua help_tags<CR>", desc = "Help tags" },
+		{ "<leader>sk", "<cmd>FzfLua keymaps<CR>", desc = "Search keymaps" },
+		{ "<leader>sm", "<cmd>FzfLua marks<CR>", desc = "Search marks" },
+		{ "<leader>sr", "<cmd>FzfLua registers<CR>", desc = "Search registers" },
+		{ "<leader>sc", "<cmd>FzfLua command_history<CR>", desc = "Command history" },
+		
+		-- Git Integrations
+		{ "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "Git commits" },
+		{ "<leader>gC", "<cmd>FzfLua git_bcommits<CR>", desc = "Git buffer commits" },
+		{ "<leader>gb", "<cmd>FzfLua git_branches<CR>", desc = "Git branches" },
+		{ "<leader>gs", "<cmd>FzfLua git_status<CR>", desc = "Git status" },
 	},
 	after = function()
 		require("fzf-lua").setup({})
